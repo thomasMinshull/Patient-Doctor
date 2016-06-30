0baf2fce0b55fe59edc74f34e6df3d4064acfbec
Thomas Minshull <thomasminshull@gmail.com>
2016-06-30 11:34:40 -0700

    initial commit

---
+++ Patient&Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient&Doctor
+//
+//  Created by thomas minshull on 2016-06-30.
+//  Copyright © 2016 thomas minshull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
